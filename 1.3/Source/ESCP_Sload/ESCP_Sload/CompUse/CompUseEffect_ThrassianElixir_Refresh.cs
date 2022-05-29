04ab18fd36050742f28294d38d9ea3609abdf416
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-05-30 09:11:16 +1200

    Update to 1.0.16
    - Thrassian Refreshment Elixir

---
+++ 1.3/Source/ESCP_Sload/ESCP_Sload/CompUse/CompUseEffect_ThrassianElixir_Refresh.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ESCP_Sload
+{
+    class CompUseEffect_ThrassianElixir_Refresh : CompUseEffect_Artifact
+	{
+		new public CompProperties_UseEffectThrassianElixir_Refresh Props
+		{
+			get
+			{
+				return (CompProperties_UseEffectThrassianElixir_Refresh)this.props;
+			}
+		}
+
+		public override void DoEffect(Pawn usedBy)
+		{
+			base.DoEffect(usedBy);
+			if (usedBy.def == ThingDefOf.ESCP_SloadRace && usedBy.abilities.abilities.Find(x => x.def == Props.ability) != null)
+			{
+				Ability a = usedBy.abilities.abilities.Find(x => x.def == Props.ability);
+				a.StartCooldown(0);
+			}
+			else
+			{
+				if (!usedBy.RaceProps.IsMechanoid)
+				{
+					GenExplosion.DoExplosion(usedBy.Position, usedBy.Map, 3, DamageDefOf.Extinguish, usedBy, -1, -1f, null, null, null, null, ThingDefOf.ESCP_Gas_ThrassianPlague, 1f, 1, false, null, 0f, 1, 0f, false, null, null);
+					usedBy.Kill(null);
+				}
+			}
+		}
+
+		public override bool CanBeUsedBy(Pawn p, out string failReason)
+		{
+			if (p.abilities.abilities.Find(x=>x.def == Props.ability) != null)
+			{
+				Ability a = p.abilities.abilities.Find(x => x.def == Props.ability);
+				if (a.CooldownTicksRemaining > 0)
+                {
+					return base.CanBeUsedBy(p, out failReason);
+				}
+			}
+			failReason = "ESCP_Sload_AlreadyHasHediff".Translate(p.Name, this.parent.Label);
+			return false;
+		}
+	}
+}
---
+++ 1.3/Source/ESCP_Sload/ESCP_Sload/CompUse/Properties/CompProperties_UseEffectThrassianElixir_Refresh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ESCP_Sload
+{
+    class CompProperties_UseEffectThrassianElixir_Refresh : CompProperties_UseEffectArtifact
+    {
+        public CompProperties_UseEffectThrassianElixir_Refresh()
+        {
+            this.compClass = typeof(CompUseEffect_ThrassianElixir_Refresh);
+        }
+
+        public AbilityDef ability;
+    }
+}
